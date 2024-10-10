d1507a212254caa5e6c8e412143427aba4f3313b
LAPTOP-00S74NOS\諒 <ryomata35@gmail.com>
2024-10-10 15:52:06 +0900

    例外処理　課題1＋基礎

---
+++ 20241010/20241010_Prac01_又吉諒.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241010
+{
+    internal class _20241010_Prac01_又吉諒
+    {
+        static void Main(string[] args)
+        {
+            ///割り算の例外処理
+            try
+            {
+                Console.WriteLine("すうじをにゅうりょくしてください");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("すうじをにゅうりょくしてください");
+                int num2 = int.Parse(Console.ReadLine());
+                int sum = Add(num1, num2);
+                Console.WriteLine($"合計{sum}");
+              int sub = Sub(num1, num2);
+                Console.WriteLine($"差{sub}");
+                int molt= Multiplication(num1, num2);
+                Console.WriteLine($"積{molt}");
+                int result = Divide(num1, num2);
+                Console.WriteLine($"商{result}");
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.Error.WriteLine("err ０で割ろうとしています");
+                Console.Error.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.Error.WriteLine("err 数字が正しく認識されていません");
+                Console.Error.WriteLine(ex.Message);
+            }
+            finally { Console.WriteLine("計算を終了する"); }
+
+            static int Divide(int num1, int num2)
+            {
+                if (num2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return num1 / num2;
+            }
+            static int Add(int num1, int num2) 
+            {
+                return num1 + num2;
+            }
+            static int Sub(int num1, int num2) 
+            {
+                return num1 - num2;
+            }
+            static int Multiplication(int num1, int num2)
+            { 
+                return num1 * num2;
+            }
+        }
+    }
+
+}
+
---
+++ 20241010/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _20241010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///割り算の例外処理
+            try
+            {
+                Console.WriteLine("わられるすうじをにゅうりょくしてください");
+                int num1=int.Parse(Console.ReadLine());
+                Console.WriteLine("わるすうじをにゅうりょくしてください");
+                int num2=int.Parse(Console.ReadLine());
+                int result = Divide(num1, num2);
+                Console.WriteLine($"{num1}/{num2}={result}");
+
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.Error.WriteLine("err ０で割ろうとしています" );
+                Console.Error.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.Error.WriteLine("err 数字が正しく認識されていません");
+                Console.Error.WriteLine(ex.Message);
+            }
+            finally { Console.WriteLine("プログラムを終了する"); }
+
+            static int Divide(int num1, int num2) 
+            {
+                if(num2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return num1/num2;   
+            }
+
+        }
+    }
+}
+ 
